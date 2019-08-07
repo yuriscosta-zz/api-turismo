9adc1a293b9efa8de2d6ece91a6cb89ea275f71a
yuriscosta <yuri.sales@protonmail.com>
2019-08-07 00:45:49 -0300

    Add endpoints

--- atracoes/models.py
+++ atracoes/models.py
@@ -5,7 +5,7 @@ class Atracao(models.Model):
     nome = models.CharField(max_length=150)
     descricao = models.TextField()
     horario_funcionamento = models.TextField()
-    idade_minina = models.IntegerField()
+    idade_minima = models.IntegerField()
 
     def __str__(self):
         return self.nome
