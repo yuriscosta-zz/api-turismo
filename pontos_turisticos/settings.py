da167b6b466dfcb621d442ed93ff36a6a647dbc1
yuriscosta <yuri.sales@protonmail.com>
2019-08-08 02:01:29 -0300

    Add pagination

--- pontos_turisticos/settings.py
+++ pontos_turisticos/settings.py
@@ -144,3 +144,8 @@ REST_FRAMEWORK = {
 }
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 2
+}
