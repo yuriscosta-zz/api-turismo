3a28e833359321f2aca665424e539567009f2042
yuriscosta <yuri.sales@protonmail.com>
2019-08-08 02:17:06 -0300

    Add actions to aprove comments

---
+++ comentarios/actions.py
@@ -0,0 +1,10 @@
+def reprova_comentarios(modeladmin, request, queryset):
+    queryset.update(aprovado=False)
+
+
+def aprova_comentarios(modeladmin, request, queryset):
+    queryset.update(aprovado=True)
+
+
+reprova_comentarios.short_description = 'Reprovar comentários'
+aprova_comentarios.short_description = 'Aprovar comentários'
